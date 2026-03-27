0d7f1cf9f64159764213346a439ea8407579512c
Chris <211069418+chris-mech@users.noreply.github.com>
2026-03-27 11:43:46 +0000

    feat: complete task 3

--- FunctionalProgrammingSol/FunctionalProgramming/Exercises001.cs
+++ FunctionalProgrammingSol/FunctionalProgramming/Exercises001.cs
@@ -10,6 +10,6 @@ namespace FunctionalProgramming
     {
         public static Func<int, int> AddOne = num => num + 1;
         public static Func<int, int> SquareIt = num => num * num;
-
+        public static Func<int, int> AddTen = num => num + 10;
     }
 }
